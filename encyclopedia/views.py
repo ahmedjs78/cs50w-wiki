65125fdf6ae9e233c4cd422ab167907d74063142
ahmedjs78 <ahmedjs778@gmail.com>
2024-03-06 23:44:13 +0400

    making the search method give recomendations if the input dosent exicst

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -26,33 +26,57 @@ def intres(request,title):
 def search(request):
     userSearch = request.POST.get('q').lower()
     allentries_lower = [entry.lower() for entry in util.list_entries()]
-    if any(ext in userSearch for ext in allentries_lower): 
-        htmlContent =  convertMarkdown(userSearch)
-        return render(request, "encyclopedia/inter.html", {
-        "bomba":htmlContent })
-    for i in userSearch:
-        print(f"{i} for i")
-        for l in allentries_lower:
-            print(f"{l} for l")
-            if i in l :
-                print(i)
-                break  # Break out of the inner loop if a match is found
+    list = []
+    if userSearch not in allentries_lower:
+        for i in userSearch:
+            print(f"{i} for i")
+            for l in allentries_lower:
+                print(f"{l} for l")
+                if i in l:
+                
+                    list.append(l)
+                    print(f"for last loop / {list}")
             else:
                 continue
             break
         else:
-            htmlContent =  convertMarkdown(l)
-            return render(request, "encyclopedia/inter.html", {
-            "bomba":htmlContent })
-        if userSearch in  allentries_lower:
-            util.get_entry(userSearch)
-            htmlContent =  convertMarkdown(userSearch)
-            return render(request, "encyclopedia/inter.html", {
-            "bomba":htmlContent })
-        else:
-            return render(request, "encyclopedia/inter.html", {
-            "eroorMassage":f"eroor {userSearch} is not found"
-            })
+            # This block will execute if no break occurred in the inner loop
+            htmlContent = convertMarkdown(l)
+            print(htmlContent)
+            return render(request, "encyclopedia/inter.html", {"bomba": htmlContent,"skrta": list})
+
+    # If userSearch is in allentries_lower, execute the following block
+    if userSearch in allentries_lower:
+        util.get_entry(userSearch)
+        htmlContent = convertMarkdown(userSearch)
+        return render(request, "encyclopedia/inter.html", {"bomba": htmlContent})
+
+    # userSearch = request.POST.get('q').lower()
+    # allentries_lower = [entry.lower() for entry in util.list_entries()]
+    # for i in userSearch:
+    #     print(f"{i} for i")
+    #     for l in allentries_lower:
+    #         print(f"{l} for l")
+    #         if i in l :
+    #             print(i)
+    #             break  # Break out of the inner loop if a match is found
+    #     else:
+    #         continue
+    #     break
+    # else:
+    #     htmlContent =  convertMarkdown(l)
+    #     return render(request, "encyclopedia/inter.html", {
+    #     "bomba":htmlContent })
+    
+    # if userSearch in  allentries_lower:
+    #     util.get_entry(userSearch)
+    #     htmlContent =  convertMarkdown(userSearch)
+    #     return render(request, "encyclopedia/inter.html", {
+    #     "bomba":htmlContent })
+    #     else:
+    #         return render(request, "encyclopedia/inter.html", {
+    #         "eroorMassage":f"eroor {userSearch} is not found"
+    #         })
 
 
 
